638b947ec763b7f70e45393ffdabe4afd0ab2b33
Nick LaMuro <nicklamuro@gmail.com>
2014-01-22 16:28:42 -0600

    Adds metadata.rb

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name              "wkhtmltopdf"
+maintainer        "SportNgin"
+license           "MIT"
+description       "Installs a global executable for the `wkhtmltopdf` lib."
+long_description  "Self continated recipe to build and install wkhtmltopdf 0.10.0_rc2 binary.  No external dependancies required."
+version           "0.0.1"
+
+
+recipe "wkhtmltopdf", "Installs the wkhtmltopdf 0.10.0_rc2 binary"
+
+supports "debian"
+supports "ubuntu"
+supports "centos"
+supports "redhat"
+supports "fedora"

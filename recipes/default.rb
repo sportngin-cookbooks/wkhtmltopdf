b8ac8b1b83eb9e1501f0c58f48f31ba7270dadfa
Nick LaMuro <nicklamuro@gmail.com>
2014-02-21 09:41:08 -0600

    Moves variables to top of file and fixes
    Makes use of the tmp variables that we were using in the rest of the
    commands to dry up the code a bit.
    
    Also fixes the shell outs to wkthmltopdf --version.

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,8 +1,11 @@
 # Much of this recipe is copied from https://github.com/bflad/chef-wkhtmltopdf
 
-cache_dir = Chef::Config[:file_cache_path]
-tgz_path = File.join(cache_dir, "#{node['wkhtmltopdf']['binary_extracted_name']}.tgz")
-version = node['wkhtmltopdf']['version']
+cache_dir      = Chef::Config[:file_cache_path]
+tgz_path       = File.join(cache_dir, "#{node['wkhtmltopdf']['binary_extracted_name']}.tgz")
+
+version        = node['wkhtmltopdf']['version']
+new_bin_file   = "#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
+extracted_file = "#{node['wkhtmltopdf']['binary_extracted_name']}"
 
 packages = value_for_platform_family(
   ['debian'] => %w(libxrender1 libxext6 libfontconfig1),
@@ -14,23 +17,20 @@ packages.each do |pkg|
 end
 
 cookbook_file tgz_path do
-  source "#{node['wkhtmltopdf']['binary_extracted_name']}.tgz"
+  source "#{extracted_file}.tgz"
   mode '0644'
 end
 
-execute "rm #{File.join(cache_dir, node['wkhtmltopdf']['binary_extracted_name'])}" if node['wkhtmltopdf']['force']
+execute "rm #{File.join(cache_dir, extracted_file)}" if node['wkhtmltopdf']['force']
 
 execute "Extract #{tgz_path}" do
   cwd cache_dir
   command "tar zxf #{tgz_path}"
   not_if do
-    File.exists?(File.join(cache_dir, node['wkhtmltopdf']['binary_extracted_name'])) &&
-    `#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf -- version`.include?(version.sub(/[^0-9\.].*/, ''))
+    File.exists?(File.join(cache_dir, extracted_file)) &&
+    `#{new_bin_file} --version`.include?(version.sub(/[^0-9\.].*/, ''))
   end
 end
-
-new_bin_file   = "#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
-extracted_file = "#{node['wkhtmltopdf']['binary_extracted_name']}"
 
 execute "rm #{new_bin_file}" if node['wkhtmltopdf']['force']
 
@@ -38,6 +38,6 @@ execute "cp #{extracted_file} #{new_bin_file}" do
   cwd cache_dir
   not_if do
     File.exists?(new_bin_file) &&
-    `#{new_bin_file} -- version`.include?(version.sub(/[^0-9\.].*/, ''))
+    `#{new_bin_file} --version`.include?(version.sub(/[^0-9\.].*/, ''))
   end
 end

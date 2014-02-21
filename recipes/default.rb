d4178a834b328aaedbf31a6dd5d2deae04ef2c8a
Nick LaMuro <nicklamuro@gmail.com>
2014-02-20 20:42:40 -0600

    Uses not_if to determine if install is needed
    execute#creates is run before the not_if block can be executed, and will
    and will skip running the command if the file exists.  Since that can't
    be helped, I have removed the file and replaced it with the same
    functionality in the not_if block.
    
    This also adds a version check to the installed version to make sure if
    the versions don't match, we go forth with the install.

--- recipes/default.rb
+++ recipes/default.rb
@@ -2,6 +2,7 @@
 
 cache_dir = Chef::Config[:file_cache_path]
 tgz_path = File.join(cache_dir, "#{node['wkhtmltopdf']['binary_extracted_name']}.tgz")
+version = node['wkhtmltopdf']['version']
 
 packages = value_for_platform_family(
   ['debian'] => %w(libxrender1 libxext6 libfontconfig1),
@@ -20,7 +21,10 @@ end
 execute "Extract #{tgz_path}" do
   cwd cache_dir
   command "tar zxf #{tgz_path}"
-  creates File.join(cache_dir, node['wkhtmltopdf']['binary_extracted_name'])
+  not_if do
+    File.exists?(File.join(cache_dir, node['wkhtmltopdf']['binary_extracted_name'])) &&
+    `#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf -- version`.include?(version.sub(/[^0-9\.].*/, ''))
+  end
 end
 
 new_bin_file   = "#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
@@ -28,5 +32,8 @@ extracted_file = "#{node['wkhtmltopdf']['binary_extracted_name']}"
 
 execute "cp #{extracted_file} #{new_bin_file}" do
   cwd cache_dir
-  creates new_bin_file
+  not_if do
+    File.exists?(new_bin_file) &&
+    `#{new_bin_file} -- version`.include?(version.sub(/[^0-9\.].*/, ''))
+  end
 end

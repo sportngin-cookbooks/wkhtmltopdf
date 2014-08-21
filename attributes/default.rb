e71ca043dc0a76fbf81b9ccf6a17c7dd2a8550b9
Bob Black <bob.black@sportngin.com>
2014-08-21 13:43:30 -0500

    Update attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,10 +1,9 @@
+# This should be pulled from the distro
 if node['kernel']['machine'] == 'x86_64'
-	default['wkhtmltopdf']['arch'] = 'amd64'
+	default['wkhtmltox']['arch'] = 'amd64'
 else
-	default['wkhtmltopdf']['arch'] = 'i386'
+	default['wkhtmltox']['arch'] = 'i386'
 end
 
-default['wkhtmltopdf']['install_dir'] = "/usr/local/bin"
-default['wkhtmltopdf']['version'] = '0.12.0-03c001d'
-
-default['wkhtmltopdf']['binary_extracted_name'] = "wkhtmltopdf-#{node['wkhtmltopdf']['version']}-#{node['wkhtmltopdf']['arch']}"
+default['wkhtmltox']['version'] = "0.12.1"
+default['wkhtmltox']['distro'] = "centos6"

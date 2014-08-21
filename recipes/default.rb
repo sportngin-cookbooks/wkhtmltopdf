eaec5de2f0e081635e13b4de62c6bfbc7a1a64e5
Bob Black <bob.black@sportngin.com>
2014-08-21 13:44:00 -0500

    Rework recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,43 +1,58 @@
-# Much of this recipe is copied from https://github.com/bflad/chef-wkhtmltopdf
+# I wish this worked... (it only works for symbol keys)
+# rpm_name       = "wkhtmltox-%{version}_linux-%{distro}-%{arch}.rpm" % node['wkhtmltox']
+
+wk_version     = node['wkhtmltox']['version']
+wk_arch        = node['wkhtmltox']['arch']
+wk_distro      = node['wkhtmltox']['distro']
+rpm_name       = "wkhtmltox-#{wk_version}_linux-#{wk_distro}-#{wk_arch}.rpm"
 
 cache_dir      = Chef::Config[:file_cache_path]
-tgz_path       = File.join(cache_dir, "#{node['wkhtmltopdf']['binary_extracted_name']}.tgz")
+rpm_path       = File.join(cache_dir, rpm_name)
 
-version        = node['wkhtmltopdf']['version']
-new_bin_file   = "#{node['wkhtmltopdf']['install_dir']}/wkhtmltopdf"
-extracted_file = "#{node['wkhtmltopdf']['binary_extracted_name']}"
+# dependencies from centos 6 .rpm file:
+# fontconfig freetype libpng zlib libjpeg
+# openssl libX11 libXext libXrender libstdc++
+
+# satisfied by fontconfig-devel
+# fontconfig
+# freetype (fontconfig-devel -> freetype-devel)
+#
+# satisfied by libXrender
+# libX11
+# libXext
+# libXrender
+#
+# openssl (satisfied by openssl-devel) (why did original use -devel?)
+#
+# I added these two (probably for wk..png that's included)
+# libpng
+# libjpeg
+#
+# probably installed by default (but let's make sure)
+# zlib
+# libstdc++
 
 packages = value_for_platform_family(
+  # Debian package names haven't been updaetd for "wk...pdf" -> "wk...x"
   ['debian'] => %w(libxrender1 libxext6 libfontconfig1),
-  ['fedora','rhel'] => %w(libXrender libXext urw-fonts openssl-devel fontconfig-devel)
+  ['fedora','rhel'] => %w(openssl libXrender libXext fontconfig-devel urw-fonts libpng libjpeg zlib libstdc++)
 )
 
 packages.each do |pkg|
   package pkg
 end
 
-cookbook_file tgz_path do
-  source "#{extracted_file}.tgz"
+cookbook_file rpm_path do
+  source rpm_path
+  # Is this needed?
   mode '0644'
 end
 
-execute "rm #{File.join(cache_dir, extracted_file)}" if node['wkhtmltopdf']['force']
+yum_package "wkhtmltox" do
+  action :install
+  # Is this needed with cookbook_file?
+  # source rpm_path
 
-execute "Extract #{tgz_path}" do
-  cwd cache_dir
-  command "tar zxf #{tgz_path}"
-  not_if do
-    File.exists?(File.join(cache_dir, extracted_file)) &&
-    `#{new_bin_file} --version`.include?(version.sub(/[^0-9\.].*/, ''))
-  end
-end
-
-execute "rm #{new_bin_file}" if node['wkhtmltopdf']['force']
-
-execute "cp #{extracted_file} #{new_bin_file}" do
-  cwd cache_dir
-  not_if do
-    File.exists?(new_bin_file) &&
-    `#{new_bin_file} --version`.include?(version.sub(/[^0-9\.].*/, ''))
-  end
+  # Will a version check work as a not_if would?
+  #version wk_version
 end

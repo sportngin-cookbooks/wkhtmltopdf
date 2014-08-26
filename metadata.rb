0ef42edad26bfbb06871fb6c7f6955937f0973df
Bob Black <bob.black@sportngin.com>
2014-08-26 12:56:28 -0500

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "SportNgin"
 license           "MIT"
 description       "Installs the wkhtmltox package."
 long_description  "Self continated recipe to build and install the linux wkhtmltox package.  No external dependancies required."
-version           "0.1.0"
+version           "0.2.0"
 
 
 recipe "wkhtmltox", "Installs the linux wkhtmltox binary"

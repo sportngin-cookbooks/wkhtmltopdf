01777ef3ccf015b39eac297e197290ffd228b619
Bob Black <bob.black@sportngin.com>
2014-08-26 22:44:37 -0500

    Tweak error text

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,7 @@
 cache_dir    = Chef::Config[:file_cache_path]
 package_file = node['wkhtmltox']['package_file']
 log "package_file: #{package_file}"
-raise "No package_file (This distro + release is not supported.)" unless package_file
+raise "No package_file (This platform + platform_version is not supported.)" unless package_file
 package_path = File.join(cache_dir, package_file)
 
 cookbook_file package_path do

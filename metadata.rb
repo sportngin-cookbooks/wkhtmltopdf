5341166c8f76eb44b9fb6e13c228c6bb08f16e20
Bob Black <bob.black@sportngin.com>
2014-09-07 21:29:29 -0500

    Drop (commented out) debian / ubuntu lines

--- metadata.rb
+++ metadata.rb
@@ -5,11 +5,8 @@ description       "Installs the wkhtmltox package."
 long_description  "Self continated recipe to build and install the linux wkhtmltox package.  No external dependancies required."
 version           "0.2.0"
 
-
 recipe "wkhtmltox", "Installs the linux wkhtmltox binary"
 
-#supports "debian"
-#supports "ubuntu"
 supports "centos"
 supports "redhat"
 supports "fedora"
